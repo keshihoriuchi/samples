365fac7a02810fe151c82e92bff43cdf783b3135
Takeshi Horiuchi <keshihoriuchi@gmail.com>
2021-07-24 04:47:17 +0900

    Commit generated code

---
+++ elixir/registry_and_dynamic_supervisor/lib/session_manager.ex
@@ -0,0 +1,18 @@
+defmodule SessionManager do
+  @moduledoc """
+  Documentation for `SessionManager`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SessionManager.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ elixir/registry_and_dynamic_supervisor/lib/session_manager/session_worker.ex
@@ -0,0 +1,3 @@
+defmodule SessionManager.SessionWorker do
+
+end
