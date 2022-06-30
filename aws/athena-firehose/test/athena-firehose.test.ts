e8dc43a1c053de8dd7526d2099893581085908bd
Takeshi Horiuchi <keshihoriuchi@gmail.com>
2022-07-01 08:38:22 +0900

    init

---
+++ aws/athena-firehose/test/athena-firehose.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AthenaFirehose from '../lib/athena-firehose-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/athena-firehose-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AthenaFirehose.AthenaFirehoseStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
