59ba7b3b243bb4eeaa3250a681d36a3db82ea750
Takeshi Horiuchi <keshihoriuchi@gmail.com>
2023-05-03 14:50:49 +0900

    Move directory

--- ts/svelte-login/login-no-dep/src/lib/server/constants.ts
+++
@@ -1,5 +0,0 @@
-export const REDIRECT_URI = 'http://localhost:5173/login/google/callback';
-// Googleのサイトから正式な値を取得してくること
-export const CLIENT_ID = 'xxxx';
-export const CLIENT_SECRET = 'xxxx';
-export const COOKIE_NAME = 'session_id';
